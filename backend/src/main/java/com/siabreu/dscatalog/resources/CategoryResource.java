479be45e0ab24335bcbd38393a63d3e28687bda6
siabreu <siabreu@gmail.com>
2021-09-11 18:15:25 -0300

    Inserindo nova categoria

--- backend/src/main/java/com/siabreu/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/siabreu/dscatalog/resources/CategoryResource.java
@@ -1,16 +1,19 @@
 package com.siabreu.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.siabreu.dscatalog.dto.CategoryDTO;
-import com.siabreu.dscatalog.entities.Category;
 import com.siabreu.dscatalog.services.CategoryService;
 
 @RestController
@@ -34,5 +37,11 @@ public class CategoryResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
